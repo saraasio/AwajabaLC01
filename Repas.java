df9e31504778d7982ee4d8aba48e37beaa31f933
Sara <sara.irharrabine.sio@gmail.com>
2026-09-07 10:40:11 +0200

    C2

--- Repas.java
+++ Repas.java
@@ -3,6 +3,11 @@ public class Repas {
 	private int numero ; 
 	private LocalDate date ; 
 	
-	
+	public Repas (int numero, LocalDate date) {
+		super();
+		 this.numero = numero ;
+		 this.date = date ;
+		 
+	}
 	
 }
